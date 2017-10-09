f01d48b2aad5e253bbc422da035eb53ef97f6c36
Wahid Ali <wahidalimohammed@outlook.com>
2017-10-09 15:33:20 -0500

    Switch Statement

---
+++ SwitchStatement.java
@@ -0,0 +1,19 @@
+class SwitchStatement{
+	public static void main(String args[]){
+	int age;
+	age = 3;
+	switch (age){
+	case 1:
+	System.out.println("You can crawl");
+	break;
+	case 2:
+	System.out.println("You can walk");
+	break;
+	case 3:
+	System.out.println("You can run");
+	break;
+	default:
+	System.out.println("You can boss");
+	}
+     }
+}
