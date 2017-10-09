3fb02cc0449f4718f10aafbc9a25e59caaf23d43
Wahid Ali <wahidalimohammed@outlook.com>
2017-10-09 15:57:51 -0500

    Program without break

---
+++ SwitchStatement1.java
@@ -0,0 +1,19 @@
+class SwitchStatement1{
+	public static void main(String args[]){
+	int age;
+	age = 2;
+	switch (age){
+	case 1:
+	System.out.println("You can crawl");
+	
+	case 2:
+	System.out.println("You can walk");
+	
+	case 3:
+	System.out.println("You can run");
+
+	default:
+	System.out.println("You are boss");
+	}
+     }
+}
